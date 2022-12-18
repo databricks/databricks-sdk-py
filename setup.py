f76cc0ad76b0eadb6e797032448d37d2ebfe1a6f
Serge Smertin <259697+nfx@users.noreply.github.com>
2022-12-18 13:00:12 +0100

    Added basic GitHub release automation (#7)
    This PR adds a basic GitHub release with wheel upload

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ __version__ = "0.0.1"
 # look at this one
 # https://github.com/psf/requests/blob/main/setup.py
 
-setup(name="databricks-sdk-py",
+setup(name="databricks",
       version=__version__,
-      packages=find_packages(),
+      packages=find_packages(exclude=["tests", "*tests.*", "*tests"]),
       python_requires=">=3.7",
       install_requires=["requests>=2.28.1", ],
-      extras_require={"dev": ["pytest", "pytest-cov", "yapf", "pycodestyle", "autoflake", "isort"]},
+      extras_require={"dev": ["pytest", "pytest-cov", "yapf", "pycodestyle", "autoflake", "isort", "wheel"]},
       author="Serge Smertin <serge.smertin@databricks.com",
-      description="Databricks SDK (Official) for Python",
+      description="Ofiicial Databricks SDK for Python",
       long_description=io.open("README.md", encoding="utf-8").read(),
       classifiers=[
           "Intended Audience :: Developers", "Intended Audience :: System Administrators",
