606926a2f54a0ee6d32cfaa720bfa271861c2c07
Boyuan Deng <40481626+dby-tmwctw@users.noreply.github.com>
2023-05-10 03:23:47 -0700

    Preserve original `databricks.sdk.runtime` for internal purposes (#96)
    ## Changes
    We want to use `databricks-sdk` in DBR but currently installing it
    breaks existing `databricks.sdk.runtime` module we added. This PR fix it
    by using the internal packages if existed, and fall-back to OSS
    implementation if that does not exist.
    
    ## Tests
    - Manual testing on E2 Dogfood
    
    - [ ] `make test` run locally
    - [x] `make fmt` applied
    - [ ] relevant integration tests applied
    
    ---------
    
    Signed-off-by: Boyuan Deng <40481626+dby-tmwctw@users.noreply.github.com>
    Co-authored-by: Kartik Gupta <88345179+kartikgupta-db@users.noreply.github.com>

--- databricks/sdk/runtime/__init__.py
+++ databricks/sdk/runtime/__init__.py
@@ -1,24 +1,31 @@
-def _get_global_dbutils():
-    try:
-        global dbutils
-        return dbutils
-    except NameError:
-        return None
+is_local_implementation = True
 
-
-dbutils = _get_global_dbutils()
+# All objects that are injected into the Notebook's user namespace should also be made
+# available to be imported from databricks.sdk.runtime.globals. This import can be used
+# in Python modules so users can access these objects from Files more easily.
+dbruntime_objects = [
+    "display", "displayHTML", "dbutils", "table", "sql", "udf", "getArgument", "sc", "sqlContext", "spark"
+]
 
 try:
-    from .stub import *
+    # Internal implementation
+    from dbruntime import UserNamespaceInitializer
+
+    userNamespaceGlobals = UserNamespaceInitializer.getOrCreate().get_namespace_globals()
+    _globals = globals()
+    for var in dbruntime_objects:
+        _globals[var] = userNamespaceGlobals[var]
+    is_local_implementation = False
 except ImportError:
-    from databricks.sdk.dbutils import RemoteDbUtils
-    if not dbutils:
-        # this assumes that all environment variables are set
-        dbutils = RemoteDbUtils()
-except NameError:
-    from databricks.sdk.dbutils import RemoteDbUtils
-    if not dbutils:
+    # OSS implementation
+    is_local_implementation = True
+
+    try:
+        from .stub import *
+    except (ImportError, NameError):
+        from databricks.sdk.dbutils import RemoteDbUtils
+
         # this assumes that all environment variables are set
         dbutils = RemoteDbUtils()
 
-__all__ = ['dbutils']
+__all__ = ['dbutils'] if is_local_implementation else dbruntime_objects
