5901a00c2b173bbf4ee61f454e2f60ee988eae98
Rafael Pierre <rafael.pierre@databricks.com>
2023-05-22 14:03:46 +0200

    user agent

--- databricks/sdk/core.py
+++ databricks/sdk/core.py
@@ -742,7 +742,7 @@ class ApiClient:
 
         self._cfg = cfg
         self._debug_truncate_bytes = cfg.debug_truncate_bytes if cfg.debug_truncate_bytes else 96
-        self._user_agent_base = cfg.user_agent if cfg.user_agent else None
+        self._user_agent_base = cfg.user_agent
 
         retry_strategy = Retry(
             total=6,
