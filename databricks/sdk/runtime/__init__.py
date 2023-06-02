e7c79ac6d2ca417f8c24decd398420b341a85f9f
Boyuan Deng <boyuan.deng@databricks.com>
2023-06-02 17:38:18 -0400

    Testing Changes.

--- databricks/sdk/runtime/__init__.py
+++ databricks/sdk/runtime/__init__.py
@@ -7,8 +7,20 @@ dbruntime_objects = [
     "display", "displayHTML", "dbutils", "table", "sql", "udf", "getArgument", "sc", "sqlContext", "spark"
 ]
 
+# DO NOT MOVE THE TRY-CATCH BLOCK BELOW AND DO NOT ADD THINGS BEFORE IT! WILL MAKE TEST FAIL.
+try:
+    # We don't want to expose additional entity to user namespace, so
+    # a workaround here for exposing required information in notebook environment
+    from dbruntime.sdk_credential_provider import init_runtime_native_auth
+    dbruntime_objects.append("init_runtime_native_auth")
+except ImportError:
+    init_runtime_native_auth = None
+
+globals()["init_runtime_native_auth"] = init_runtime_native_auth
+
 try:
     # Internal implementation
+    # Separated from above for backward compatibility
     from dbruntime import UserNamespaceInitializer
 
     userNamespaceGlobals = UserNamespaceInitializer.getOrCreate().get_namespace_globals()
@@ -29,15 +41,5 @@ except ImportError:
 
         # this assumes that all environment variables are set
         dbutils = RemoteDbUtils()
-
-try:
-    # We don't want to expose additional entity to user namespace, so
-    # a workaround here for exposing required information in notebook environment
-    from dbruntime.sdk_credential_provider import init_runtime_native_auth
-    dbruntime_objects.append("init_runtime_native_auth")
-except ImportError:
-    init_runtime_native_auth = None
-
-globals()["init_runtime_native_auth"] = init_runtime_native_auth
 
 __all__ = ['dbutils'] if is_local_implementation else dbruntime_objects
