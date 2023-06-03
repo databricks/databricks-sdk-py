97212ef023179e652f0fcead55fbfea1b9abf082
Boyuan Deng <40481626+dby-tmwctw@users.noreply.github.com>
2023-06-03 06:41:36 -0700

    Exposing runtime credential provider without changing user namespace (#140)
    ## Changes
    Per discussion
    [here](https://databricks.slack.com/archives/C050SV0NJ72/p1685132954243759),
    we need to expose the `init_runtime_native_auth` without modifying the
    user namespace. Both verified working with latest universe and not
    making the Python SDK explode in previous DBR.
    
    ## Tests
    <!-- 
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    
    - [ ] `make test` run locally
    - [x] `make fmt` applied
    - [x] relevant integration tests applied

--- databricks/sdk/runtime/__init__.py
+++ databricks/sdk/runtime/__init__.py
@@ -1,24 +1,26 @@
-from typing import Callable, Dict, Tuple
-
 is_local_implementation = True
 
 # All objects that are injected into the Notebook's user namespace should also be made
 # available to be imported from databricks.sdk.runtime.globals. This import can be used
 # in Python modules so users can access these objects from Files more easily.
 dbruntime_objects = [
-    "display", "displayHTML", "dbutils", "table", "sql", "udf", "getArgument", "sc", "sqlContext", "spark",
-    "init_runtime_native_auth"
+    "display", "displayHTML", "dbutils", "table", "sql", "udf", "getArgument", "sc", "sqlContext", "spark"
 ]
 
-RuntimeAuth = Tuple[str, Callable[[], Dict[str, str]]]
+# DO NOT MOVE THE TRY-CATCH BLOCK BELOW AND DO NOT ADD THINGS BEFORE IT! WILL MAKE TEST FAIL.
+try:
+    # We don't want to expose additional entity to user namespace, so
+    # a workaround here for exposing required information in notebook environment
+    from dbruntime.sdk_credential_provider import init_runtime_native_auth
+    dbruntime_objects.append("init_runtime_native_auth")
+except ImportError:
+    init_runtime_native_auth = None
 
-
-def init_runtime_native_auth() -> RuntimeAuth:
-    raise NotImplementedError
-
+globals()["init_runtime_native_auth"] = init_runtime_native_auth
 
 try:
     # Internal implementation
+    # Separated from above for backward compatibility
     from dbruntime import UserNamespaceInitializer
 
     userNamespaceGlobals = UserNamespaceInitializer.getOrCreate().get_namespace_globals()
