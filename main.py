29236d647ecfb32712d052bc4f3159949237b2ae
Kartik Gupta <88345179+kartikgupta-db@users.noreply.github.com>
2023-06-16 12:09:25 +0200

    Delete main.py
    Signed-off-by: Kartik Gupta <88345179+kartikgupta-db@users.noreply.github.com>

--- main.py
+++
@@ -1,5 +0,0 @@
-from databricks.connect import DatabricksSession
-from databricks.sdk.core import Config
-
-config = Config(host='https://x', token='x', product="xapp", product_version="0.0.0")
-spark = DatabricksSession().builder.userAgent("custom-agent").getOrCreate()
