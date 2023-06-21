2d9da3fb24128eb669c8efd5bcfb8abca8151c8b
Kartik Gupta <88345179+kartikgupta-db@users.noreply.github.com>
2023-06-21 13:30:27 +0200

    Create a deepcopy of config in api client (#172)
    ## Changes
    * We do not want the config to change after API client has been
    initialised. One API client should be linked to 1 set of Config
    variables ONLY.
    
    - [x] `make test` run locally
    - [x] `make fmt` applied
    - [x] relevant integration tests applied
    
    ---------
    
    Signed-off-by: Kartik Gupta <88345179+kartikgupta-db@users.noreply.github.com>

--- databricks/sdk/__init__.py
+++ databricks/sdk/__init__.py
@@ -107,7 +107,7 @@ class WorkspaceClient:
                                    debug_headers=debug_headers,
                                    product=product,
                                    product_version=product_version)
-        self.config = config
+        self.config = config.copy()
         self.dbutils = dbutils.RemoteDbUtils(self.config)
         self.api_client = client.ApiClient(self.config)
         self.files = FilesMixin(self.api_client)
@@ -214,7 +214,7 @@ class AccountClient:
                                    debug_headers=debug_headers,
                                    product=product,
                                    product_version=product_version)
-        self.config = config
+        self.config = config.copy()
         self.api_client = client.ApiClient(self.config)
         self.access_control = AccountAccessControlAPI(self.api_client)
         self.billable_usage = BillableUsageAPI(self.api_client)
