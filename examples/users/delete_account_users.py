30876672a972460f099dc5092b2fde8863c7f4c3
Miles Yucht <miles@databricks.com>
2023-07-18 13:44:11 +0200

    Updated account-level and workspace-level user management examples (#241)
    ## Changes
    Add SCIM examples for updating a user via the PATCH API at the account
    level.
    
    This PR reruns the OpenAPI generator from
    https://github.com/databricks/databricks-sdk-go/pull/540. This ensures
    that enum names are handled correctly in the example optimizer, and it
    also includes one additional bugfix to enum value parsing causing
    `URN_IETF_PARAMS_SCIM_API_MESSAGES20_PATCH_OP` to be renamed to
    `URN_IETF_PARAMS_SCIM_API_MESSAGES_2_0_PATCH_OP`.
    
    ## Tests
    <!-- 
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    
    - [ ] `make test` run locally
    - [ ] `make fmt` applied
    - [ ] relevant integration tests applied

---
+++ examples/users/delete_account_users.py
@@ -0,0 +1,9 @@
+import time
+
+from databricks.sdk import AccountClient
+
+a = AccountClient()
+
+user = a.users.create(display_name=f'sdk-{time.time_ns()}', user_name=f'sdk-{time.time_ns()}@example.com')
+
+a.users.delete(delete=user.id)
