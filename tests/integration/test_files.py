7bc98c8219ae2502a3e37f1f3cc4d6540b94e321
Miles Yucht <miles@databricks.com>
2023-08-30 11:32:53 +0200

    skip obviously slow chunk sizes

--- tests/integration/test_files.py
+++ tests/integration/test_files.py
@@ -264,7 +264,7 @@ def test_files_api_download_benchmark(ucws, random):
             w.files.upload(target_file, f)
 
             totals = {}
-            for chunk_size_kb in [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]:
+            for chunk_size_kb in [20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, None]:
                 # Hack to set the chunk size
                 total = 0
                 for i in range(10):
