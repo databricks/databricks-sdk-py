a26e05ae2a2ffa8016713e6cb1e1a1de579d39f8
Serge Smertin <serge.smertin@databricks.com>
2023-10-10 17:07:58 +0200

    fmt

--- databricks/sdk/core.py
+++ databricks/sdk/core.py
@@ -1098,7 +1098,7 @@ class ApiClient:
                 "does not have any associated worker environments", "There is no worker environment with id",
                 "Unknown worker environment", "ClusterNotReadyException", "Unexpected error",
                 "Please try again later or try a faster operation.",
-                "RPC token bucket limit has been exceeded",
+                "RPC token bucket limit has been exceeded", "exceeded the concurrent limit of",
             ]
             for substring in transient_error_string_matches:
                 if substring not in message:
