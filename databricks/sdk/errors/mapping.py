47bd23ee0b41bf3112199afa892128ff06f7ba94
Serge Smertin <serge.smertin@databricks.com>
2023-11-10 17:17:45 +0100

    ..

--- databricks/sdk/errors/mapping.py
+++ databricks/sdk/errors/mapping.py
@@ -3,10 +3,10 @@
 from .base import DatabricksError
 
 __all__ = [
-    'error_mapper', 'BadRequest', 'Unauthenticated', 'PermissionDenied', 'NotFound', 'ResourceConflict',
-    'TooManyRequests', 'Cancelled', 'InternalError', 'NotImplemented', 'TemporarilyUnavailable',
-    'DeadlineExceeded', 'InvalidParameterValue', 'Aborted', 'AlreadyExists', 'ResourceAlreadyExists',
-    'ResourceExhausted', 'RequestLimitExceeded', 'Unknown', 'DataLoss'
+    'BadRequest', 'Unauthenticated', 'PermissionDenied', 'NotFound', 'ResourceConflict', 'TooManyRequests',
+    'Cancelled', 'InternalError', 'NotImplemented', 'TemporarilyUnavailable', 'DeadlineExceeded',
+    'InvalidParameterValue', 'Aborted', 'AlreadyExists', 'ResourceAlreadyExists', 'ResourceExhausted',
+    'RequestLimitExceeded', 'Unknown', 'DataLoss'
 ]
 
 
@@ -14,15 +14,15 @@ class BadRequest(DatabricksError):
     """the request is invalid"""
 
 
-class Unauthenticated(DatabricksError, PermissionError):
+class Unauthenticated(DatabricksError):
     """the request does not have valid authentication (AuthN) credentials for the operation"""
 
 
-class PermissionDenied(DatabricksError, PermissionError):
+class PermissionDenied(DatabricksError):
     """the caller does not have permission to execute the specified operation"""
 
 
-class NotFound(DatabricksError, LookupError):
+class NotFound(DatabricksError):
     """the operation was performed on a resource that does not exist"""
 
 
@@ -30,7 +30,7 @@ class ResourceConflict(DatabricksError):
     """maps to all HTTP 409 (Conflict) responses"""
 
 
-class TooManyRequests(DatabricksError, ResourceWarning):
+class TooManyRequests(DatabricksError):
     """maps to HTTP code: 429 Too Many Requests"""
 
 
@@ -42,7 +42,7 @@ class InternalError(DatabricksError):
     """some invariants expected by the underlying system have been broken"""
 
 
-class NotImplemented(DatabricksError, NotImplementedError):
+class NotImplemented(DatabricksError):
     """the operation is not implemented or is not supported/enabled in this service"""
 
 
@@ -50,7 +50,7 @@ class TemporarilyUnavailable(DatabricksError):
     """the service is currently unavailable"""
 
 
-class DeadlineExceeded(DatabricksError, TimeoutError):
+class DeadlineExceeded(DatabricksError):
     """the deadline expired before the operation could complete"""
 
 
@@ -87,7 +87,7 @@ class DataLoss(InternalError):
     """unrecoverable data loss or corruption"""
 
 
-_STATUS_CODE_MAPPING = {
+STATUS_CODE_MAPPING = {
     400: BadRequest,
     401: Unauthenticated,
     403: PermissionDenied,
@@ -101,7 +101,7 @@ _STATUS_CODE_MAPPING = {
     504: DeadlineExceeded,
 }
 
-_ERROR_CODE_MAPPING = {
+ERROR_CODE_MAPPING = {
     'INVALID_PARAMETER_VALUE': InvalidParameterValue,
     'ABORTED': Aborted,
     'ALREADY_EXISTS': AlreadyExists,
@@ -111,20 +111,3 @@ _ERROR_CODE_MAPPING = {
     'UNKNOWN': Unknown,
     'DATA_LOSS': DataLoss,
 }
-
-
-def error_mapper(status_code: int, raw: dict) -> DatabricksError:
-    error_code = raw.get('error_code', None)
-    if error_code in _ERROR_CODE_MAPPING:
-        # more specific error codes override more generic HTTP status codes
-        return _ERROR_CODE_MAPPING[error_code](**raw)
-
-    if status_code in _STATUS_CODE_MAPPING:
-        # more generic HTTP status codes matched after more specific error codes,
-        # where there's a default exception class per HTTP status code, and we do
-        # rely on Databricks platform exception mapper to do the right thing.
-        return _STATUS_CODE_MAPPING[status_code](**raw)
-
-    # backwards-compatible error creation for cases like using older versions of
-    # the SDK on way never releases of the platform.
-    return DatabricksError(**raw)
