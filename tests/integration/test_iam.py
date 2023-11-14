a03dcc36c48e5981c2925ec1194467e7db50de12
Miles Yucht <miles@databricks.com>
2023-11-14 12:29:13 +0100

    address comments

--- tests/integration/test_iam.py
+++ tests/integration/test_iam.py
@@ -16,8 +16,8 @@ def test_scim_error_unmarshall(w, random):
 
 
 @pytest.mark.parametrize("path,call", [
-    ("/api/2.0/preview/scim/v2/Users", lambda w: w.users.list(count=1)),
-    ("/api/2.0/preview/scim/v2/Groups", lambda w: w.groups.list(count=1)),
+    ("/api/2.0/preview/scim/v2/Users", lambda w: w.users.list(count=10)),
+    ("/api/2.0/preview/scim/v2/Groups", lambda w: w.groups.list(count=4)),
     ("/api/2.0/preview/scim/v2/ServicePrincipals", lambda w: w.service_principals.list(count=1)),
 ])
 def test_workspace_users_list_pagination(w, path, call):
@@ -29,10 +29,8 @@ def test_workspace_users_list_pagination(w, path, call):
 
 
 @pytest.mark.parametrize("path,call", [
-    # there are ~7k users in our aws prod account
-    ("/api/2.0/accounts/%s/scim/v2/Users", lambda a: a.users.list(count=1000)),
-    ("/api/2.0/accounts/%s/scim/v2/Groups", lambda a: a.groups.list(count=1)),
-    # there are ~3k service principals in our aws prod account
+    ("/api/2.0/accounts/%s/scim/v2/Users", lambda a: a.users.list(count=3000)),
+    ("/api/2.0/accounts/%s/scim/v2/Groups", lambda a: a.groups.list(count=5)),
     ("/api/2.0/accounts/%s/scim/v2/ServicePrincipals", lambda a: a.service_principals.list(count=1000)),
 ])
 def test_account_users_list_pagination(a, path, call):
