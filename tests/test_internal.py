9cbe153718a2db272db48ed99764bc1ad2035b2c
Miles Yucht <miles@databricks.com>
2023-11-21 09:48:04 +0100

    fmt

--- tests/test_internal.py
+++ tests/test_internal.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 from enum import Enum
 
-import pytest
+from databricks.sdk.service._internal import (_enum, _from_dict,
+                                              _repeated_dict, _repeated_enum)
 
-from databricks.sdk.service._internal import _enum, _from_dict, _repeated_dict, _repeated_enum
 
 class A(Enum):
     a = 'a'
@@ -13,11 +13,14 @@ class A(Enum):
 def test_enum():
     assert _enum({'field': 'a'}, 'field', A) == A.a
 
+
 def test_enum_unknown():
     assert _enum({'field': 'c'}, 'field', A) is None
 
+
 def test_repeated_enum():
     assert _repeated_enum({'field': ['a', 'b']}, 'field', A) == [A.a, A.b]
+
 
 def test_repeated_enum_unknown():
     assert _repeated_enum({'field': ['a', 'c']}, 'field', A) == [A.a]
@@ -31,8 +34,10 @@ class B:
     def from_dict(cls, d: dict) -> 'B':
         return cls(d['field'])
 
+
 def test_from_dict():
     assert _from_dict({'x': {'field': 'a'}}, 'x', B) == B('a')
+
 
 def test_repeated_dict():
     assert _repeated_dict({'x': [{'field': 'a'}, {'field': 'c'}]}, 'x', B) == [B('a'), B('c')]
