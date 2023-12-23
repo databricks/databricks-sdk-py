44fd10fd175fc63f8ce9a6098abeb0cb2807f1ea
Hendryk Weiland <hendryk.weiland@hhu.de>
2023-12-23 17:12:29 +0100

    Fix execution examples, run fmte

--- examples/command_context/execute_commands.py
+++ examples/command_context/execute_commands.py
@@ -2,4 +2,4 @@ from databricks.sdk import WorkspaceClient
 
 w = WorkspaceClient()
 
-res = w.command_context.execute("print(1)")
+res = w.command_execution.execute(command="print(1)")
--- examples/command_executor/execute_commands.py
+++ examples/command_executor/execute_commands.py
@@ -1,10 +1,11 @@
 import os
 
 from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.compute import Language
 
 w = WorkspaceClient()
 
 cluster_id = w.clusters.ensure_cluster_is_running(
     os.environ["DATABRICKS_CLUSTER_ID"]) and os.environ["DATABRICKS_CLUSTER_ID"]
 
-res = w.command_executor.execute(cluster_id, "python", "print(1)")
+res = w.command_execution.execute(cluster_id=cluster_id, language=Language.PYTHON, command="print(1)")
