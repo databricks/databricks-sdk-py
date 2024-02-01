1e9788dc0542d9cd108fb37aee26ab1b4494c9cf
Miles Yucht <miles@databricks.com>
2024-02-01 09:44:48 +0100

    fmt

--- tests/integration/test_settings.py
+++ tests/integration/test_settings.py
@@ -5,4 +5,3 @@ def test_workspace_conf(w):
     w.workspace_conf.set_status({"enableResultsDownloading": "true"})
     conf = w.workspace_conf.get_status(keys="enableResultsDownloading")
     assert conf["enableResultsDownloading"] == "true"
-
