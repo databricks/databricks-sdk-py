0c4142875272efcd26be50ba07e4cc77bf35f61d
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2024-02-07 12:55:07 +0100

    Skip test_get_workspace_client in Azure and GCP

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_get_workspace_client(a):
     if a.config.is_azure or a.config.is_gcp:
-        pytest.skip('not available on Azure and GCP currently')
+        pytest.skip('Not available on Azure and GCP currently')
     wss = list(a.workspaces.list())
     if len(wss) == 0:
         pytest.skip("no workspaces")
