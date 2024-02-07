a57b7e022d14c567a583eb91b98699c80519c224
Tanmay Rustagi <88379306+tanmay-db@users.noreply.github.com>
2024-02-07 13:10:08 +0100

    Skip test_get_workspace_client in Azure and GCP (#531)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    We skip this test since it doesn't work currently in Azure and GCP
    
    ## Tests
    <!-- 
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    
    - [ ] `make test` run locally
    - [ ] `make fmt` applied
    - [ ] relevant integration tests applied

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -2,6 +2,8 @@ import pytest
 
 
 def test_get_workspace_client(a):
+    if a.config.is_azure or a.config.is_gcp:
+        pytest.skip('Not available on Azure and GCP currently')
     wss = list(a.workspaces.list())
     if len(wss) == 0:
         pytest.skip("no workspaces")
