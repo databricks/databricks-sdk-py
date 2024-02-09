56c4bb25e97544104d0c845fdda7a6c4df8247a2
Miles Yucht <miles@databricks.com>
2024-02-09 11:31:03 +0100

    add to init

--- examples/clusters/select_spark_version_clusters_api_integration.py
+++ examples/clusters/select_spark_version_clusters_api_integration.py
@@ -2,4 +2,4 @@ from databricks.sdk import WorkspaceClient
 
 w = WorkspaceClient()
 
-latest = w.clusters.select_spark_version(latest=True)
+latest = w.clusters.select_spark_version(latest=True, long_term_support=True)
