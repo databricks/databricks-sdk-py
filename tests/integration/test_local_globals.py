62cb41590ce37636eb6ef9406421619b771eb1fa
kartikgupta-db <kartik.gupta@databricks.com>
2024-02-22 19:21:28 +0100

    rename

--- tests/integration/test_local_globals.py
+++ tests/integration/test_local_globals.py
@@ -1,11 +1,11 @@
-def test_runtime_spark(w, env_or_skip):
+def test_local_global_spark(w, env_or_skip):
     env_or_skip("SPARK_CONNECT_CLUSTER_ID")
 
     from databricks.sdk.runtime import spark
     assert spark.sql("SELECT 1").collect()[0][0] == 1
 
 
-def test_runtime_display(w, env_or_skip):
+def test_local_global_display(w, env_or_skip):
     from databricks.sdk.runtime import display, displayHTML
 
     # assert no errors
