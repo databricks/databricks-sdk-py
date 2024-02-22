7ce8fa358b692d7589252fdfd6077d5d8ab1a20e
kartikgupta-db <kartik.gupta@databricks.com>
2024-02-22 19:43:20 +0100

    fix test

--- tests/integration/test_local_globals.py
+++ tests/integration/test_local_globals.py
@@ -1,8 +1,25 @@
-def test_local_global_spark(w, env_or_skip):
-    env_or_skip("SPARK_CONNECT_CLUSTER_ID")
+from contextlib import contextmanager
 
-    from databricks.sdk.runtime import spark
-    assert spark.sql("SELECT 1").collect()[0][0] == 1
+
+@contextmanager
+def restorable_env():
+    import os
+    current_env = os.environ.copy()
+
+    try:
+        yield
+    finally:
+        os.environ.clear()
+        os.environ.update(current_env)
+
+
+def test_local_global_spark(w, env_or_skip):
+    cluster_id = env_or_skip("SPARK_CONNECT_CLUSTER_ID")
+    with restorable_env():
+        import os
+        os.environ["DATABRICKS_CLUSTER_ID"] = cluster_id
+        from databricks.sdk.runtime import spark
+        assert spark.sql("SELECT 1").collect()[0][0] == 1
 
 
 def test_local_global_display(w, env_or_skip):
