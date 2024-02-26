87989f15988f81090fc063e462171db5972886ec
kartikgupta-db <kartik.gupta@databricks.com>
2024-02-26 19:47:48 +0100

    fix no error test

--- tests/integration/test_dbconnect.py
+++ tests/integration/test_dbconnect.py
@@ -4,6 +4,29 @@ DBCONNECT_DBR_CLIENT = {"13.3": "13.3.3", "14.3": "14.3.1", }
 
 
 @pytest.fixture(scope="function")
+def reload_modules():
+    """
+    Returns a function that can be used to reload modules. This is useful when testing
+    Databricks Connect, since both the `databricks.connect` and `databricks.sdk.runtime` modules
+    are stateful, and we need to reload these modules to reset the state cache between test runs.
+    """
+    import importlib
+    import sys
+
+    def reload(name: str):
+        v = sys.modules.get(name)
+        if v is None:
+            return
+        try:
+            print(f"Reloading {name}")
+            importlib.reload(v)
+        except Exception as e:
+            print(f"Failed to reload {name}: {e}")
+
+    return reload
+
+
+@pytest.fixture(scope="function")
 def restorable_env():
     import os
     current_env = os.environ.copy()
@@ -35,19 +58,26 @@ def setup_dbconnect_test(request, env_or_skip, restorable_env):
 
 
 @pytest.mark.xdist_group(name="databricks-connect")
-def test_dbconnect_initialisation(w, setup_dbconnect_test):
+def test_dbconnect_initialisation(w, setup_dbconnect_test, reload_modules):
+    reload_modules("databricks.connect")
     from databricks.connect import DatabricksSession
+    reload_modules("databricks.connect")
+
     spark = DatabricksSession.builder.getOrCreate()
     assert spark.sql("SELECT 1").collect()[0][0] == 1
 
 
 @pytest.mark.xdist_group(name="databricks-connect")
-def test_dbconnect_runtime_import(w, setup_dbconnect_test):
+def test_dbconnect_runtime_import(w, setup_dbconnect_test, reload_modules):
+    reload_modules("databricks.sdk.runtime")
     from databricks.sdk.runtime import spark
+
     assert spark.sql("SELECT 1").collect()[0][0] == 1
 
 
 @pytest.mark.xdist_group(name="databricks-connect")
-def test_dbconnect_runtime_import_no_error_if_doesnt_exist(w):
+def test_dbconnect_runtime_import_no_error_if_doesnt_exist(w, reload_modules):
+    reload_modules("databricks.sdk.runtime")
     from databricks.sdk.runtime import spark
+
     assert spark is None
