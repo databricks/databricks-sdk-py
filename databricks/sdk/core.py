45a356ca24ce92c80f06c3f60f9969aaf25987e4
Miles Yucht <miles@databricks.com>
2024-04-03 14:56:58 +0200

    Override INVALID_PARAMETER_VALUE on fetching non-existent job/cluster (#591)
    ## Changes
    Port of https://github.com/databricks/databricks-sdk-go/pull/864 to the
    Python SDK.
    
    Most services use RESOURCE_DOES_NOT_EXIST error code with 404 status
    code to indicate that a resource doesn't exist. However, for legacy
    reasons, Jobs and Clusters services use INVALID_PARAMETER_VALUE error
    code with 400 status code instead. This makes tools like Terraform and
    UCX difficult to maintain, as these services need different error
    handling logic. However, we can't change these behaviors as customers
    already depend on the raw HTTP response status & contents.
    
    This PR corrects these errors in the SDK itself. SDK users can then do
    
    ```
    try:
        w.jobs.get_by_id('abc')
    except ResourceDoesNotExist:
        pass
    ```
    just as you would expect from other resources.
    
    Updated the README with more information about this as well.
    
    ## Tests
    <!-- 
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    
    - [ ] `make test` run locally
    - [ ] `make fmt` applied
    - [ ] relevant integration tests applied

--- databricks/sdk/core.py
+++ databricks/sdk/core.py
@@ -264,7 +264,7 @@ class ApiClient:
         if is_too_many_requests_or_unavailable:
             kwargs['retry_after_secs'] = self._parse_retry_after(response)
         kwargs['message'] = message
-        return error_mapper(status_code, kwargs)
+        return error_mapper(response, kwargs)
 
     def _record_request_log(self, response: requests.Response, raw=False):
         if not logger.isEnabledFor(logging.DEBUG):
