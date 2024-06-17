a4ca06841e07507faf9463bc94a247082c1c8b5a
Parth Bansal <parth.bansal@databricks.com>
2024-06-17 18:05:41 +0200

    added test for host url format check

--- tests/test_config.py
+++ tests/test_config.py
@@ -15,3 +15,17 @@ def test_config_supports_legacy_credentials_provider():
     c2 = c.copy()
     assert c2._product == 'foo'
     assert c2._product_version == '1.2.3'
+
+
+def test_config_host_url_format_check(mocker):
+    mocker.patch('databricks.sdk.config.Config.init_auth')
+
+    c1 = Config(host="abc.def.ghi/")
+    c2 = Config(host="https://abc.def.ghi/")
+    c3 = Config(host="https://abc.def.ghi")
+    c4 = Config(host="abc.def.ghi")
+
+    assert c1.host == "https://abc.def.ghi"
+    assert c2.host == "https://abc.def.ghi"
+    assert c3.host == "https://abc.def.ghi"
+    assert c4.host == "https://abc.def.ghi"
