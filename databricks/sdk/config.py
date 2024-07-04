7a49922224752654b2497e00cd5618cd9a7b2a17
Miles Yucht <miles@databricks.com>
2024-07-04 17:31:16 +0200

    Support partners in SDK (#648)
    ## Changes
    Ports https://github.com/databricks/databricks-sdk-go/pull/925 to the
    Python SDK.
    
    Partners of Databricks need a mechanism to register themselves in
    libraries or applications that they write. In this way, requests made by
    users of those libraries will include sufficient information to link
    those requests to the original users.
    
    This PR adds a new `useragent` module with functions to manipulate the
    user agent.
    * `product()`: returns the globally configured product & version.
    * `with_product(product: str, product_version: str)`: configure the
    global product & version.
    * `extra()`: returns the globally configured extra user agent metadata.
    * `with_extra(key: str, value: str)`: add an extra entry to the global
    extra user agent metadata.
    * `with_partner(partner: str)`: add a partner to the global extra user
    agent metadata.
    * `to_string(product_override: Optional[Tuple[str, str]]=None,
    other_info: Optional[List[Tuple[str, str]]] = None): str`: return the
    User-Agent header as a string.
    
    
    One new function here is `with_partner`, which can be used by a partner
    to add partner information to the User-Agent header for requests made by
    the SDK. The new header will have the form `partner/<parther id>`. The
    partner identifier is opaque for the SDK, but it must be alphanumeric.
    
    This PR also removes the requirement that a user agent entry contain
    only a single copy of each key. This allows multiple partners to
    register in the same library or application.
    
    In this PR, I've also refactored the user agent library to be more
    static, aligning it with the Go and Java SDKs. This makes it easier to
    maintain and ensures similar behavior between all 3 SDKs. Note that this
    SDK has extra functionality that doesn't exist in the Go and Java SDKs,
    namely config-level user agent info; that is preserved here.
    
    ## Tests
    Unit tests were added to verify that the user agent contains all
    expected parts and supports multiple partners.
    
    - [ ] `make test` run locally
    - [ ] `make fmt` applied
    - [ ] relevant integration tests applied

--- databricks/sdk/config.py
+++ databricks/sdk/config.py
@@ -3,19 +3,18 @@ import copy
 import logging
 import os
 import pathlib
-import platform
 import sys
 import urllib.parse
-from typing import Dict, Iterable, List, Optional, Tuple
+from typing import Dict, Iterable, Optional
 
 import requests
 
+from . import useragent
 from .clock import Clock, RealClock
 from .credentials_provider import CredentialsStrategy, DefaultCredentials
 from .environments import (ALL_ENVS, AzureEnvironment, Cloud,
                            DatabricksEnvironment, get_environment_for_hostname)
 from .oauth import OidcEndpoints, Token
-from .version import __version__
 
 logger = logging.getLogger('databricks.sdk')
 
@@ -44,30 +43,14 @@ class ConfigAttribute:
         return f"<ConfigAttribute '{self.name}' {self.transform.__name__}>"
 
 
-_DEFAULT_PRODUCT_NAME = 'unknown'
-_DEFAULT_PRODUCT_VERSION = '0.0.0'
-_STATIC_USER_AGENT: Tuple[str, str, List[str]] = (_DEFAULT_PRODUCT_NAME, _DEFAULT_PRODUCT_VERSION, [])
-
-
 def with_product(product: str, product_version: str):
     """[INTERNAL API] Change the product name and version used in the User-Agent header."""
-    global _STATIC_USER_AGENT
-    prev_product, prev_version, prev_other_info = _STATIC_USER_AGENT
-    logger.debug(f'Changing product from {prev_product}/{prev_version} to {product}/{product_version}')
-    _STATIC_USER_AGENT = product, product_version, prev_other_info
+    useragent.with_product(product, product_version)
 
 
 def with_user_agent_extra(key: str, value: str):
     """[INTERNAL API] Add extra metadata to the User-Agent header when developing a library."""
-    global _STATIC_USER_AGENT
-    product_name, product_version, other_info = _STATIC_USER_AGENT
-    for item in other_info:
-        if item.startswith(f"{key}/"):
-            # ensure that we don't have duplicates
-            other_info.remove(item)
-            break
-    other_info.append(f"{key}/{value}")
-    _STATIC_USER_AGENT = product_name, product_version, other_info
+    useragent.with_extra(key, value)
 
 
 class Config:
@@ -111,21 +94,12 @@ class Config:
                  # Deprecated. Use credentials_strategy instead.
                  credentials_provider: CredentialsStrategy = None,
                  credentials_strategy: CredentialsStrategy = None,
-                 product=_DEFAULT_PRODUCT_NAME,
-                 product_version=_DEFAULT_PRODUCT_VERSION,
+                 product=None,
+                 product_version=None,
                  clock: Clock = None,
                  **kwargs):
         self._header_factory = None
         self._inner = {}
-        # as in SDK for Go, pull information from global static user agent context,
-        # so that we can track additional metadata for mid-stream libraries, as well
-        # as for cases, when the downstream product is used as a library and is not
-        # configured with a proper product name and version.
-        static_product, static_version, _ = _STATIC_USER_AGENT
-        if product == _DEFAULT_PRODUCT_NAME:
-            product = static_product
-        if product_version == _DEFAULT_PRODUCT_VERSION:
-            product_version = static_version
         self._user_agent_other_info = []
         if credentials_strategy and credentials_provider:
             raise ValueError(
@@ -147,8 +121,7 @@ class Config:
             self._fix_host_if_needed()
             self._validate()
             self.init_auth()
-            self._product = product
-            self._product_version = product_version
+            self._init_product(product, product_version)
         except ValueError as e:
             message = self.wrap_debug_info(str(e))
             raise ValueError(message) from e
@@ -260,47 +233,19 @@ class Config:
     @property
     def user_agent(self):
         """ Returns User-Agent header used by this SDK """
-        py_version = platform.python_version()
-        os_name = platform.uname().system.lower()
 
-        ua = [
-            f"{self._product}/{self._product_version}", f"databricks-sdk-py/{__version__}",
-            f"python/{py_version}", f"os/{os_name}", f"auth/{self.auth_type}",
-        ]
-        if len(self._user_agent_other_info) > 0:
-            ua.append(' '.join(self._user_agent_other_info))
-        # as in SDK for Go, pull information from global static user agent context,
-        # so that we can track additional metadata for mid-stream libraries. this value
-        # is shared across all instances of Config objects intentionally.
-        _, _, static_info = _STATIC_USER_AGENT
-        if len(static_info) > 0:
-            ua.append(' '.join(static_info))
-        if len(self._upstream_user_agent) > 0:
-            ua.append(self._upstream_user_agent)
-        if 'DATABRICKS_RUNTIME_VERSION' in os.environ:
-            runtime_version = os.environ['DATABRICKS_RUNTIME_VERSION']
-            if runtime_version != '':
-                runtime_version = self._sanitize_header_value(runtime_version)
-                ua.append(f'runtime/{runtime_version}')
-
-        return ' '.join(ua)
-
-    @staticmethod
-    def _sanitize_header_value(value: str) -> str:
-        value = value.replace(' ', '-')
-        value = value.replace('/', '-')
-        return value
+        # global user agent includes SDK version, product name & version, platform info,
+        # and global extra info. Config can have specific extra info associated with it,
+        # such as an override product, auth type, and other user-defined information.
+        return useragent.to_string(self._product_info,
+                                   [("auth", self.auth_type)] + self._user_agent_other_info)
 
     @property
     def _upstream_user_agent(self) -> str:
-        product = os.environ.get('DATABRICKS_SDK_UPSTREAM', None)
-        product_version = os.environ.get('DATABRICKS_SDK_UPSTREAM_VERSION', None)
-        if product is not None and product_version is not None:
-            return f"upstream/{product} upstream-version/{product_version}"
-        return ""
+        return " ".join(f"{k}/{v}" for k, v in useragent._get_upstream_user_agent_info())
 
     def with_user_agent_extra(self, key: str, value: str) -> 'Config':
-        self._user_agent_other_info.append(f"{key}/{value}")
+        self._user_agent_other_info.append((key, value))
         return self
 
     @property
@@ -504,6 +449,13 @@ class Config:
                 raise ValueError('not configured')
         except ValueError as e:
             raise ValueError(f'{self._credentials_strategy.auth_type()} auth: {e}') from e
+
+    def _init_product(self, product, product_version):
+        if product is not None or product_version is not None:
+            default_product, default_version = useragent.product()
+            self._product_info = (product or default_product, product_version or default_version)
+        else:
+            self._product_info = None
 
     def __repr__(self):
         return f'<{self.debug_string()}>'
--- tests/test_config.py
+++ tests/test_config.py
@@ -2,24 +2,17 @@ import platform
 
 import pytest
 
+from databricks.sdk import useragent
 from databricks.sdk.config import Config, with_product, with_user_agent_extra
 from databricks.sdk.version import __version__
 
 from .conftest import noop_credentials
 
 
-def test_config_copy_preserves_product_and_product_version():
-    c = Config(credentials_strategy=noop_credentials, product='foo', product_version='1.2.3')
-    c2 = c.copy()
-    assert c2._product == 'foo'
-    assert c2._product_version == '1.2.3'
-
-
 def test_config_supports_legacy_credentials_provider():
     c = Config(credentials_provider=noop_credentials, product='foo', product_version='1.2.3')
     c2 = c.copy()
-    assert c2._product == 'foo'
-    assert c2._product_version == '1.2.3'
+    assert c2._product_info == ('foo', '1.2.3')
 
 
 @pytest.mark.parametrize('host,expected', [("https://abc.def.ghi", "https://abc.def.ghi"),
@@ -54,7 +47,7 @@ def test_extra_and_upstream_user_agent(monkeypatch):
 
     assert config.user_agent == (
         f"test/0.0.0 databricks-sdk-py/{__version__} python/3.0.0 os/testos auth/basic"
-        f" test-extra-1/1 test-extra-2/2 upstream/upstream-product upstream-version/0.0.1"
+        " test-extra-1/1 test-extra-2/2 upstream/upstream-product upstream-version/0.0.1"
         " runtime/13.1-anything-else")
 
     with_product('some-product', '0.32.1')
@@ -76,6 +69,8 @@ def test_config_copy_deep_copies_user_agent_other_info(config):
     assert "test/test2" in config_copy.user_agent
     assert "test/test2" not in config.user_agent
 
+    original_extra = useragent.extra()
     with_user_agent_extra("blueprint", "0.4.6")
     assert "blueprint/0.4.6" in config.user_agent
     assert "blueprint/0.4.6" in config_copy.user_agent
+    useragent._reset_extra(original_extra)
