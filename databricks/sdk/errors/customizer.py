2ecfa797b5644cecb775c717a1b9e4b0b545e852
Miles Yucht <miles@databricks.com>
2024-09-13 15:14:50 +0200

    fix typo

--- databricks/sdk/errors/customizer.py
+++ databricks/sdk/errors/customizer.py
@@ -15,7 +15,7 @@ class _ErrorCustomizer(abc.ABC):
 class _RetryAfterCustomizer(_ErrorCustomizer):
     """An error customizer that sets the retry_after_secs parameter based on the Retry-After header."""
 
-    _DEFALT_RETRY_AFTER_SECONDS = 1
+    _DEFAULT_RETRY_AFTER_SECONDS = 1
     """The default number of seconds to wait before retrying a request if the Retry-After header is missing or is not
     a valid integer."""
 
@@ -24,11 +24,11 @@ class _RetryAfterCustomizer(_ErrorCustomizer):
         retry_after = response.headers.get("Retry-After")
         if retry_after is None:
             logging.debug(
-                f'No Retry-After header received in response with status code 429 or 503. Defaulting to {cls._DEFALT_RETRY_AFTER_SECONDS}'
+                f'No Retry-After header received in response with status code 429 or 503. Defaulting to {cls._DEFAULT_RETRY_AFTER_SECONDS}'
             )
             # 429 requests should include a `Retry-After` header, but if it's missing,
             # we default to 1 second.
-            return cls._DEFALT_RETRY_AFTER_SECONDS
+            return cls._DEFAULT_RETRY_AFTER_SECONDS
         # If the request is throttled, try parse the `Retry-After` header and sleep
         # for the specified number of seconds. Note that this header can contain either
         # an integer or a RFC1123 datetime string.
@@ -40,10 +40,10 @@ class _RetryAfterCustomizer(_ErrorCustomizer):
             return int(retry_after)
         except ValueError:
             logging.debug(
-                f'Invalid Retry-After header received: {retry_after}. Defaulting to {cls._DEFALT_RETRY_AFTER_SECONDS}'
+                f'Invalid Retry-After header received: {retry_after}. Defaulting to {cls._DEFAULT_RETRY_AFTER_SECONDS}'
             )
             # defaulting to 1 sleep second to make self._is_retryable() simpler
-            return cls._DEFALT_RETRY_AFTER_SECONDS
+            return cls._DEFAULT_RETRY_AFTER_SECONDS
 
     def customize_error(self, response: requests.Response, kwargs: dict):
         if response.status_code in (429, 503):
