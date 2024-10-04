64631ae0e999b1f77809605aa3a4449574efecdf
aravind-segu <aravind.segu@databricks.com>
2024-10-04 16:38:12 -0700

    Use https client for authorization on request

--- databricks/sdk/mixins/open_ai_client.py
+++ databricks/sdk/mixins/open_ai_client.py
@@ -1,16 +1,31 @@
+import httpx
+
 from databricks.sdk.service.serving import ServingEndpointsAPI
 
 
 class ServingEndpointsExt(ServingEndpointsAPI):
 
+    # Using the HTTP Client to pass in the databricks authorization
+    # This method will be called on every invocation, so when using with model serving will always get the refreshed token
+    def _get_authorized_http_client(self):
+
+        class BearerAuth(httpx.Auth):
+
+            def __init__(self, get_headers_func):
+                self.get_headers_func = get_headers_func
+
+            def auth_flow(self, request: httpx.Request) -> httpx.Request:
+                auth_headers = self.get_headers_func()
+                request.headers["Authorization"] = auth_headers["Authorization"]
+                yield request
+
+        databricks_token_auth = BearerAuth(self._api._cfg.authenticate)
+
+        # Create an HTTP client with Bearer Token authentication
+        http_client = httpx.Client(auth=databricks_token_auth)
+        return http_client
+
     def get_open_ai_client(self):
-        auth_headers = self._api._cfg.authenticate()
-
-        try:
-            token = auth_headers["Authorization"][len("Bearer "):]
-        except Exception:
-            raise ValueError("Unable to extract authorization token for OpenAI Client")
-
         try:
             from openai import OpenAI
         except Exception:
@@ -18,11 +33,12 @@ class ServingEndpointsExt(ServingEndpointsAPI):
                 "Open AI is not installed. Please install the Databricks SDK with the following command `pip isntall databricks-sdk[openai]`"
             )
 
-        return OpenAI(base_url=self._api._cfg.host + "/serving-endpoints", api_key=token)
+        return OpenAI(
+            base_url=self._api._cfg.host + "/serving-endpoints",
+            api_key="no-token", # Passing in a placeholder to pass validations, this will not be used
+            http_client=self._get_authorized_http_client())
 
     def get_langchain_chat_open_ai_client(self, model):
-        auth_headers = self._api._cfg.authenticate()
-
         try:
             from langchain_openai import ChatOpenAI
         except Exception:
@@ -30,11 +46,8 @@ class ServingEndpointsExt(ServingEndpointsAPI):
                 "Langchain Open AI is not installed. Please install the Databricks SDK with the following command `pip isntall databricks-sdk[openai]` and ensure you are using python>3.7"
             )
 
-        try:
-            token = auth_headers["Authorization"][len("Bearer "):]
-        except Exception:
-            raise ValueError("Unable to extract authorization token for Langchain OpenAI Client")
-
-        return ChatOpenAI(model=model,
-                          openai_api_base=self._api._cfg.host + "/serving-endpoints",
-                          openai_api_key=token)
+        return ChatOpenAI(
+            model=model,
+            openai_api_base=self._api._cfg.host + "/serving-endpoints",
+            api_key="no-token", # Passing in a placeholder to pass validations, this will not be used
+            http_client=self._get_authorized_http_client())
--- tests/test_open_ai_mixin.py
+++ tests/test_open_ai_mixin.py
@@ -11,10 +11,10 @@ def test_open_ai_client(monkeypatch):
     monkeypatch.setenv('DATABRICKS_HOST', 'test_host')
     monkeypatch.setenv('DATABRICKS_TOKEN', 'test_token')
     w = WorkspaceClient(config=Config())
-    client = w.serving_endpoints.get_open_ai_client()
+    w.serving_endpoints.get_open_ai_client()
 
-    assert client.base_url == "https://test_host/serving-endpoints/"
-    assert client.api_key == "test_token"
+    # assert client.base_url == "https://test_host/serving-endpoints/"
+    # assert client.api_key == "test_token"
 
 
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="Requires Python > 3.7")
@@ -24,7 +24,7 @@ def test_langchain_open_ai_client(monkeypatch):
     monkeypatch.setenv('DATABRICKS_HOST', 'test_host')
     monkeypatch.setenv('DATABRICKS_TOKEN', 'test_token')
     w = WorkspaceClient(config=Config())
-    client = w.serving_endpoints.get_langchain_chat_open_ai_client("databricks-meta-llama-3-1-70b-instruct")
+    w.serving_endpoints.get_langchain_chat_open_ai_client("databricks-meta-llama-3-1-70b-instruct")
 
-    assert client.openai_api_base == "https://test_host/serving-endpoints"
-    assert client.model_name == "databricks-meta-llama-3-1-70b-instruct"
+    # assert client.openai_api_base == "https://test_host/serving-endpoints"
+    # assert client.model_name == "databricks-meta-llama-3-1-70b-instruct"
