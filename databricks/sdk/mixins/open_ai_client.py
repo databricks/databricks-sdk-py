deda35dafca2b8c6b8b70ba858eb55cd4bb82829
aravind-segu <aravind.segu@databricks.com>
2024-10-03 21:26:09 -0700

    Add Langchain Open AI Client

--- databricks/sdk/mixins/open_ai_client.py
+++ databricks/sdk/mixins/open_ai_client.py
@@ -3,7 +3,7 @@ from databricks.sdk.service.serving import ServingEndpointsAPI
 
 class ServingEndpointsExt(ServingEndpointsAPI):
 
-    def get_open_api_client(self):
+    def get_open_ai_client(self):
         auth_headers = self._api._cfg.authenticate()
 
         try:
@@ -13,3 +13,16 @@ class ServingEndpointsExt(ServingEndpointsAPI):
 
         from openai import OpenAI
         return OpenAI(base_url=self._api._cfg.host + "/serving-endpoints", api_key=token)
+
+    def get_langchain_chat_open_ai_client(self, model):
+        auth_headers = self._api._cfg.authenticate()
+
+        try:
+            token = auth_headers["Authorization"][len("Bearer "):]
+        except Exception:
+            raise ValueError("Unable to extract authorization token for Langchain OpenAI Client")
+
+        from langchain_openai import ChatOpenAI
+        return ChatOpenAI(model=model,
+                          openai_api_base=self._api._cfg.host + "/serving-endpoints",
+                          openai_api_key=token)
--- tests/test_open_ai_mixin.py
+++ tests/test_open_ai_mixin.py
@@ -7,7 +7,19 @@ def test_open_ai_client(monkeypatch):
     monkeypatch.setenv('DATABRICKS_HOST', 'test_host')
     monkeypatch.setenv('DATABRICKS_TOKEN', 'test_token')
     w = WorkspaceClient(config=Config())
-    client = w.serving_endpoints.get_open_api_client()
+    client = w.serving_endpoints.get_open_ai_client()
 
     assert client.base_url == "https://test_host/serving-endpoints/"
     assert client.api_key == "test_token"
+
+
+def test_langchain_open_ai_client(monkeypatch):
+    from databricks.sdk import WorkspaceClient
+
+    monkeypatch.setenv('DATABRICKS_HOST', 'test_host')
+    monkeypatch.setenv('DATABRICKS_TOKEN', 'test_token')
+    w = WorkspaceClient(config=Config())
+    client = w.serving_endpoints.get_langchain_chat_open_ai_client("databricks-meta-llama-3-1-70b-instruct")
+
+    assert client.openai_api_base == "https://test_host/serving-endpoints"
+    assert client.model_name == "databricks-meta-llama-3-1-70b-instruct"
