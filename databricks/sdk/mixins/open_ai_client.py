675c45b76a2068dff09ce4351d8062a95af107be
aravind-segu <aravind.segu@databricks.com>
2024-10-03 17:02:56 -0700

    [ML-45784]Add open ai client mixin

---
+++ databricks/sdk/mixins/open_ai_client.py
@@ -0,0 +1,16 @@
+from databricks.sdk.service.serving import ServingEndpointsAPI
+
+class ServingEndpointsExt(ServingEndpointsAPI):
+    def get_open_api_client(self):
+        auth_headers = self._api._cfg.authenticate()
+
+        try:
+            token = auth_headers["Authorization"][len("Bearer "):]
+        except Exception:
+            raise ValueError("Unable to extract authorization token for OpenAI Client")
+
+        from openai import OpenAI
+        return OpenAI(
+            base_url=self._api._cfg.host + "/serving-endpoints", 
+            api_key=token
+        )
---
+++ tests/test_open_ai_mixin.py
@@ -0,0 +1,13 @@
+import pytest
+from databricks.sdk.core import Config
+
+def test_open_ai_client(monkeypatch):
+    from databricks.sdk import WorkspaceClient
+
+    monkeypatch.setenv('DATABRICKS_HOST', 'test_host')
+    monkeypatch.setenv('DATABRICKS_TOKEN', 'test_token')
+    w = WorkspaceClient(config=Config())
+    client = w.serving_endpoints.get_open_api_client()
+
+    assert client.base_url == "https://test_host/serving-endpoints/"
+    assert client.api_key == "test_token"
