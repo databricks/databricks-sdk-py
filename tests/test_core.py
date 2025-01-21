13fef9638b9e9391bbf905dac9b11f180f200eb5
Renaud Hartert <renaud.hartert@databricks.com>
2025-01-21 20:35:10 +0100

    Adjust core test too

--- tests/test_core.py
+++ tests/test_core.py
@@ -178,6 +178,10 @@ def test_extra_and_upstream_user_agent(monkeypatch):
         def system(self):
             return 'TestOS'
 
+    # Clear all environment variables.
+    for k in os.environ:
+        monkeypatch.delenv(k, raising=False)
+
     monkeypatch.setattr(platform, 'python_version', lambda: '3.0.0')
     monkeypatch.setattr(platform, 'uname', MockUname)
     monkeypatch.setenv('DATABRICKS_SDK_UPSTREAM', "upstream-product")
