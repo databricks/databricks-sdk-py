66a752f47cc45768adccbc11b6c94a7661e458e7
Parth Bansal <parth.bansal@databricks.com>
2025-04-09 08:57:09 +0000

    update

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -1,4 +1,4 @@
-#TODO: need to decide if we want get_workspace_id on every client
+# TODO: need to decide if we want get_workspace_id on every client
 # def test_get_workspace_id(ucws, env_or_skip):
 #     ws_id = int(env_or_skip("THIS_WORKSPACE_ID"))
 #     assert ucws.get_workspace_id() == ws_id
