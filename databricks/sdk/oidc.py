f19ed8ad4471fe9691dc49d7a067654de231633b
Ubuntu <renaud.hartert@databricks.com>
2025-05-23 13:43:58 +0000

    Format

--- databricks/sdk/oidc.py
+++ databricks/sdk/oidc.py
@@ -110,7 +110,7 @@ class FileIdTokenSource(IdTokenSource):
         """
         if not self.path:
             raise ValueError("Missing path")
-        
+
         token = None
         try:
             with open(self.path, "r") as f:
@@ -119,7 +119,7 @@ class FileIdTokenSource(IdTokenSource):
             raise ValueError(f"File {self.path!r} does not exist")
         except Exception as e:
             raise ValueError(f"Error reading token file: {str(e)}")
-        
+
         if not token:
             raise ValueError(f"File {self.path!r} is empty")
         return IdToken(jwt=token)
